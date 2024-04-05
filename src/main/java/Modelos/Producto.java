8175a8a790e1a93885cef802e713a17d965b6b15
eduarfonseca <fonsecaeduar136@gmail.com>
2024-04-05 17:36:59 -0400

    gag

---
+++ src/main/java/Modelos/Producto.java
@@ -0,0 +1,61 @@
+package Modelos;
+
+public class Producto {
+   private int id;
+   private String title;
+   private double price;
+   private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    private String stock;
+
+   private String rating;
+}
+
